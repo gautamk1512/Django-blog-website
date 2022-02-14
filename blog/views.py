863f2cfe358397fc43b18518b97ff9a4fe23e634
gautamk1512 <gautamk1512@gmail.com>
2022-02-15 00:32:41 +0530

    intial commit

---
+++ blog/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, HttpResponse
+from blog.models import Post
+
+# Create your views here.
+
+
+def blogHome(request):
+    allPosts = Post.objects.all()
+    context = {'allPosts': allPosts}
+    print(allPosts)
+    return render(request, "blog/blogHome.html", context)
+
+
+def blogPost(request, slug):
+    post = Post.objects.filter(slug=slug).first()
+    print(post)
+    context = {'post': post}
+
+    return render(request, 'blog/blogPost.html', context)
+
